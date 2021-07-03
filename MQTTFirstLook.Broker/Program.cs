3b584814b2a92a22a13a90fd1d7331101109b401
Eduardo julião <eduardo.juliaojr@outlook.com>
2021-07-03 19:49:32 +0100

    Initial Commit

---
+++ MQTTFirstLook.Broker/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using MQTTnet;
+using MQTTnet.Server;
+using Serilog;
+
+namespace MQTTFirstLook.Broker
+{
+    class Program
+    {
+        private static int MessageCounter = 0;
+
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            MqttServerOptionsBuilder options = new MqttServerOptionsBuilder()
+                .WithDefaultEndpoint()
+                .WithDefaultEndpointPort(707)
+                .WithConnectionValidator(OnNewConnection)
+                .WithApplicationMessageInterceptor(OnNewMessage);
+
+
+            IMqttServer mqttServer = new MqttFactory().CreateMqttServer();
+
+            mqttServer.StartAsync(options.Build()).GetAwaiter().GetResult();
+            Console.ReadLine();
+        }
+
+        public static void OnNewConnection(MqttConnectionValidatorContext context)
+        {
+            Log.Logger.Information(
+                    "New connection: ClientId = {clientId}, Endpoint = {endpoint}, CleanSession = {cleanSession}",
+                    context.ClientId,
+                    context.Endpoint,
+                    context.CleanSession);
+        }
+
+        public static void OnNewMessage(MqttApplicationMessageInterceptorContext context)
+        {
+            var payload = context.ApplicationMessage?.Payload == null ? null : Encoding.UTF8.GetString(context.ApplicationMessage?.Payload);
+
+            MessageCounter++;
+
+            Log.Logger.Information(
+                "MessageId: {MessageCounter} - TimeStamp: {TimeStamp} -- Message: ClientId = {clientId}, Topic = {topic}, Payload = {payload}, QoS = {qos}, Retain-Flag = {retainFlag}",
+                MessageCounter,
+                DateTime.Now,
+                context.ClientId,
+                context.ApplicationMessage?.Topic,
+                payload,
+                context.ApplicationMessage?.QualityOfServiceLevel,
+                context.ApplicationMessage?.Retain);
+        }
+    }
+}
---
+++ MQTTFirstLook.Client/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Receiving;
+using MQTTnet.Extensions.ManagedClient;
+using Newtonsoft.Json;
+using Serilog;
+
+namespace MQTTFirstLook.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            MqttClientOptionsBuilder builder = new MqttClientOptionsBuilder()
+                                        .WithClientId("Dev.To")
+                                        .WithTcpServer("localhost", 707);
+
+            ManagedMqttClientOptions options = new ManagedMqttClientOptionsBuilder()
+                                    .WithAutoReconnectDelay(TimeSpan.FromSeconds(60))
+                                    .WithClientOptions(builder.Build())
+                                    .Build();
+
+            IManagedMqttClient _mqttClient = new MqttFactory().CreateManagedMqttClient();
+
+            _mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnConnected);
+            _mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(OnDisconnected);
+            _mqttClient.ConnectingFailedHandler = new ConnectingFailedHandlerDelegate(OnConnectingFailed);
+
+            _mqttClient.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(a => {
+                Log.Logger.Information("Message recieved: {payload}", a.ApplicationMessage);
+            });
+
+            _mqttClient.StartAsync(options).GetAwaiter().GetResult();
+
+            while (true)
+            {
+                string json = JsonConvert.SerializeObject(new { message = "Heyo :)", sent= DateTimeOffset.UtcNow });
+                _mqttClient.PublishAsync("dev.to/topic/json", json);
+
+                Task.Delay(1000).GetAwaiter().GetResult();
+            }
+        }
+
+        public static void OnConnected(MqttClientConnectedEventArgs obj)
+        {
+            Log.Logger.Information("Successfully connected.");
+        }
+
+        public static void OnConnectingFailed(ManagedProcessFailedEventArgs obj)
+        {
+            Log.Logger.Warning("Couldn't connect to broker.");
+        }
+
+        public static void OnDisconnected(MqttClientDisconnectedEventArgs obj)
+        {
+            Log.Logger.Information("Successfully disconnected.");
+        }
+    }
+}
